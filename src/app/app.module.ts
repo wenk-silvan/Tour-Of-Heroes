1c779ec2bc62a554e80634a9f1e3b7d26642ad86
GLOBAL\wenksi <silvan.wenk@ch.schindler.com>
2017-07-17 12:59:04 +0200

    Services (pt4)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import {HeroService} from './hero.service';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import { HeroDetailComponent } from './hero-detail.component';
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
